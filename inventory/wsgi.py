88a3e2ce885614952d191fdf0040b3a69758c563
Harry Huhn <harryhuhn@icloud.com>
2023-11-13 15:26:27 -0700

    adding files

--- inventory/wsgi.py
+++ inventory/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory.settings')
+from whitenoise import WhiteNoise
+from pathlib import Path
+from whitenoise import WhiteNoise
 
 application = get_wsgi_application()
+BASE_DIR = Path(__file__).resolve().parent.parent
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'build/static'))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory.settings')
